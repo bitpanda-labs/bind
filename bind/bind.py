51a1f393a61e580477681b75975d40bebed6c9c0
Danny McDonald <danny@Sysadmins-MacBook-Pro.local>
2018-10-29 15:22:07 +0100

    keep one-liner, fix empty string handling

--- bind/bind.py
+++ bind/bind.py
@@ -4,4 +4,4 @@ Bind: a one-line Python module to mimic os.path.join without reverting to root
 
 from urllib.parse import quote_plus
 
-def bind(*args, sep='/', url=True, safe=':/', **kwargs): return sep.join([quote_plus(str(i).strip(sep), safe=safe, **kwargs) if url else str(i).strip(sep) for i in (args[0] if isinstance(args[0], (list, set, tuple)) else args)])
+def bind(*args, sep='/', url=True, safe=':/', **kwargs): return sep.join([quote_plus(str(i).strip(sep), safe=safe, **kwargs) if url else str(i).strip(sep) for i in (args[0] if isinstance(args[0], (list, set, tuple)) else (i for i in args if i))])
