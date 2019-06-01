24616305fa1b67e534978d16c60e4edef5410d1b
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2019-06-01 14:43:20 +0200

    Fix version in __init__

--- bind/__init__.py
+++ bind/__init__.py
@@ -1,3 +1,3 @@
 from .bind import bind  # noqa
 
-__version__ = '5.0.0'  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = '2.0.0'  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
